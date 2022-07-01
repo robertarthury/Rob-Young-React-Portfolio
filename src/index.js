9cc43d3cd9ab445f102defb51d580285073c507b
robertarthury <robertarthury@gmail.com>
2022-07-01 13:15:06 -0400

    j

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+require('dotenv').config();
+
+ReactDOM.render( <App />,
   document.getElementById('root')
 );
+

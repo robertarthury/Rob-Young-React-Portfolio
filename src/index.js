cfa39d7ee323e63a21234f2d653e009225e4952b
robertarthury <robertarthury@gmail.com>
2022-07-01 22:14:33 -0400

    d

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 
-ReactDOM.render( <App />,
-  document.getElementById('root')
-);
 
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+)
